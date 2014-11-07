0ee620259c4b7f3788da453a4f4a61965c79ec2b
bluemonster122 <boo122333@gmail.com>
2014-11-07 20:19:37 +0000

    I added Stuff

---
+++ src/main/java/com/erock112233/TUM/TUM.java
@@ -0,0 +1,26 @@
+package com.erock112233.TUM;
+
+import com.erock112233.TUM.reference.refs;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = refs.MOD_ID, name = refs.MOD_NAME, version = refs.VERSION)
+public class TUM {
+
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent PreEvent){
+		
+	}
+	@EventHandler
+	public static void Init(FMLInitializationEvent event){
+		
+	}
+	@EventHandler
+	public static void PostInit(FMLPostInitializationEvent PostEvent){
+		
+	}
+}
---
+++ src/main/java/com/erock112233/TUM/reference/refs.java
@@ -0,0 +1,8 @@
+package com.erock112233.TUM.reference;
+
+public class refs {
+	public static final String MOD_ID = "TUM";
+	public static final String MOD_NAME = "TUM";
+	public static final String VERSION = "1.7.10-0.001";
+
+}
