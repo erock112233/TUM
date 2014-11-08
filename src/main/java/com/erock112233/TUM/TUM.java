500f570f3b864426ca631c2e022fa3e24e061b74
bluemonster122 <boo122333@gmail.com>
2014-11-08 10:27:01 +0000

    created the mod location in the build.gradle folder and copyed the example mod template with a few modifications

---
+++ src/main/java/com/erock112233/TUM/TUM.java
@@ -0,0 +1,20 @@
+package com.erock112233.TUM;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TUM.MODID, name = TUM.MODNAME, version = TUM.VERSION)
+public class TUM {
+	public static final String MODID = "TUM";
+	public static final String MODNAME = "TUM";
+	public static final String VERSION = "0.001";
+	
+	@EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
